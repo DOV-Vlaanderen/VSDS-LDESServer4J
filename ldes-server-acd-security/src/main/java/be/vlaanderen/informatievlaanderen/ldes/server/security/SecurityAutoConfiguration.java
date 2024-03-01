4e32b638ec32cd88d3ca4fb8e82d7dacff3a78f5
Timothy De Bock <timothy.debock.git@gmail.com>
2024-03-01 12:25:27 +0100

    fix build - upgrade to api changes in snapshot dependency

--- ldes-server-acd-security/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/security/SecurityAutoConfiguration.java
+++ ldes-server-acd-security/src/main/java/be/vlaanderen/informatievlaanderen/ldes/server/security/SecurityAutoConfiguration.java
@@ -1,62 +1,49 @@
 package be.vlaanderen.informatievlaanderen.ldes.server.security;
 
 
-import be.cumuli.security.WebServerSecurityCustomizer;
-import lombok.SneakyThrows;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static be.cumuli.security.SecurityOrder.API;
+import static be.cumuli.security.SecurityOrder.BROWSER;
 
 @Slf4j
 @Configuration
 @EnableConfigurationProperties()
 @ComponentScan("be.vlaanderen.informatievlaanderen.ldes.server")
 public class SecurityAutoConfiguration {
-    @Bean
-    WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> {
-            web.ignoring()
-                    // Spring Security should completely ignore following resources
-                    .requestMatchers(
-                            "/index.html",
-                            "/i18n/**",
-                            "/images/**",
-                            "/node_modules/**",
-                            "/robots.txt",
-                            "/*.js");
-        };
-    }
-
 
     @Bean
-    WebServerSecurityCustomizer webServerSecurityCustomizer() {
-        log.info("Securing admin and actuator endpoints.");
-        return this::webServerSecurity;
-    }
-
-    @SneakyThrows
-    private HttpSecurity webServerSecurity(HttpSecurity httpSecurity) {
-        return httpSecurity
-                .securityMatcher("/**")
-                .csrf(AbstractHttpConfigurer::disable)
+    @org.springframework.core.annotation.Order(API)
+    SecurityFilterChain apiSecurityFilterchain(HttpSecurity http,
+                                               JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
                 .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(
                         auth -> {
                             // todo rol zetten
                             // Alle post request
-                            auth.requestMatchers(HttpMethod.POST).authenticated();
+                            //auth.requestMatchers(HttpMethod.POST).authenticated();
                             // alles op het admin endpoint
-                            auth.requestMatchers("/admin/**").authenticated();
+                            //auth.requestMatchers("/admin/**").authenticated();
                             // any other page
                             auth.anyRequest().permitAll();
-                        });
+                        })
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(
+                        c -> c.jwt(cj -> cj.jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .build();
     }
+
 }
 
